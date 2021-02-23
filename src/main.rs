9852d4d4cbcd6dcbf85c280968b9d7d105e4cb5e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-02-24 00:21:28 +0900

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[test]
 fn test() {
-    let word1 = vec![String::from("ab"), String::from("c")];
-    let word2 = vec![String::from("a"), String::from("bc")];
-    assert_eq!(true, Solution::array_strings_are_equal(word1, word2));
+    assert_eq!(2, Solution::num_decodings(String::from("11106")));
+    assert_eq!(0, Solution::num_decodings(String::from("0")));
+    assert_eq!(2, Solution::num_decodings(String::from("12")));
+    assert_eq!(3, Solution::num_decodings(String::from("226")));
 }
 
 fn main() {
@@ -12,7 +13,27 @@ fn main() {
 struct Solution;
 
 impl Solution {
-    pub fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
-        word1.join("") == word2.join("")
+    pub fn num_decodings(s: String) -> i32 {
+        if s.chars().nth(0).unwrap() == '0' {
+            return 0;
+        }
+        let mut dp: Vec<i32> = vec![0; s.len() + 1];
+        println!("{}", dp.len());
+        dp[0] = 1;
+        dp[1] = 1;
+        for i in 2..=s.len() {
+            let first = s[i - 1..i].parse::<i32>().unwrap();
+            let second = s[i - 2..i].parse::<i32>().unwrap();
+
+            println!("first: {}, second: {}", first, second);
+            if first >= 1 && first <= 9 {
+                dp[i] += dp[i - 1];
+            }
+            if second >= 10 && second <= 26 {
+                dp[i] += dp[i - 2];
+            }
+            println!("dp: {:?}", dp);
+        }
+        dp[s.len()]
     }
 }
