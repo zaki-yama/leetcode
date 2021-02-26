15accf9213001d4137c3b0c622988f88665da8c5
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-02-26 09:29:39 +0900

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #[test]
 fn test() {
-    assert_eq!(2, Solution::num_decodings(String::from("11106")));
-    assert_eq!(0, Solution::num_decodings(String::from("0")));
-    assert_eq!(2, Solution::num_decodings(String::from("12")));
-    assert_eq!(3, Solution::num_decodings(String::from("226")));
+    assert_eq!(5, Solution::num_trees(3));
+    assert_eq!(1, Solution::num_trees(1));
+    // ref. https://en.wikipedia.org/wiki/Catalan_number
+    assert_eq!(14, Solution::num_trees(4));
+    assert_eq!(42, Solution::num_trees(5));
+    assert_eq!(16796, Solution::num_trees(10));
 }
 
 fn main() {
@@ -13,27 +15,16 @@ fn main() {
 struct Solution;
 
 impl Solution {
-    pub fn num_decodings(s: String) -> i32 {
-        if s.chars().nth(0).unwrap() == '0' {
-            return 0;
-        }
-        let mut dp: Vec<i32> = vec![0; s.len() + 1];
-        println!("{}", dp.len());
+    pub fn num_trees(n: i32) -> i32 {
+        let mut dp: Vec<i32> = vec![0; (n + 1) as usize];
         dp[0] = 1;
         dp[1] = 1;
-        for i in 2..=s.len() {
-            let first = s[i - 1..i].parse::<i32>().unwrap();
-            let second = s[i - 2..i].parse::<i32>().unwrap();
 
-            println!("first: {}, second: {}", first, second);
-            if first >= 1 && first <= 9 {
-                dp[i] += dp[i - 1];
+        for k in 2..=n {
+            for i in 0..k {
+                dp[k as usize] += dp[i as usize] * dp[(k - i - 1) as usize];
             }
-            if second >= 10 && second <= 26 {
-                dp[i] += dp[i - 2];
-            }
-            println!("dp: {:?}", dp);
         }
-        dp[s.len()]
+        dp[n as usize]
     }
 }
