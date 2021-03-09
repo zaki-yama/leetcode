acbc7ed159f52ce42c32f3eef0a93b9f403cecb7
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-03-09 22:38:21 +0900

    update main.rs(279)

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
 #[test]
 fn test() {
-    assert_eq!(4, Solution::rob(vec![1, 2, 3, 1]));
-    assert_eq!(12, Solution::rob(vec![2, 7, 9, 3, 1]));
-    assert_eq!(104, Solution::rob(vec![1, 2, 3, 1, 1, 100, 4]));
-    assert_eq!(0, Solution::rob(vec![]));
-    assert_eq!(1, Solution::rob(vec![1, 1]));
-    assert_eq!(1, Solution::rob(vec![1]));
+    assert_eq!(3, Solution::num_squares(12));
+    assert_eq!(2, Solution::num_squares(13));
+
+    assert_eq!(1, Solution::num_squares(1));
+    assert_eq!(2, Solution::num_squares(2));
+    assert_eq!(3, Solution::num_squares(3));
+    assert_eq!(1, Solution::num_squares(4));
+    assert_eq!(2, Solution::num_squares(5));
+    assert_eq!(4, Solution::num_squares(7));
+    assert_eq!(2, Solution::num_squares(8));
+    assert_eq!(1, Solution::num_squares(9));
+    assert_eq!(2, Solution::num_squares(10));
+    assert_eq!(3, Solution::num_squares(11));
 }
 
 fn main() {
     // println!("{}", Solution::halves_are_alike(String::from(raw_str)));
+    assert_eq!(3, Solution::num_squares(12));
 }
 
 struct Solution;
 use std::cmp;
 
 impl Solution {
-    pub fn rob(nums: Vec<i32>) -> i32 {
-        if nums.len() == 0 {
-            return 0;
+    pub fn num_squares(n: i32) -> i32 {
+        let mut ans = vec![std::i32::MAX - 1; (n + 1) as usize];
+        ans[0] = 0;
+        for i in 1..=n {
+            println!("i = {}", i);
+            let mut j = 1;
+            while j * j <= i {
+                ans[i as usize] = cmp::min(ans[i as usize], ans[(i - j * j) as usize] + 1);
+                j += 1;
+            }
         }
-        let mut dp = vec![0; nums.len() + 1];
-
-        dp[0] = 0;
-        dp[1] = nums[0];
-
-        for i in 1..nums.len() {
-            println!("[{}] {}", i, nums[i]);
-            dp[i + 1] = cmp::max(dp[i - 1] + nums[i], dp[i]);
-        }
-        println!("dp: {:?}", dp);
-        dp[nums.len()]
+        ans[n as usize]
     }
 }
